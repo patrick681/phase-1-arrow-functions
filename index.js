e9b041f6696cab4adb636c078914a8b16ad43bc8
patrick <kamaupatrick332@gmail.com>
2025-03-19 16:57:55 +0300

    ,

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function for square
+  const square = (num) => num * num;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+  // Export functions (if needed for tests)
+  module.exports = { divide, square, add };
+  
